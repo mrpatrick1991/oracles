298294a6f4cf53870f022d84f83bd887d4849bf2
Matthew Patrick <64496719+mrpatrick1991@users.noreply.github.com>
2025-01-21 11:52:43 -0700

    Update file_store.rs

--- file_store/src/file_store.rs
+++ file_store/src/file_store.rs
@@ -12,7 +12,7 @@ use http::Uri;
 use std::path::Path;
 use std::str::FromStr;
 
-const prefix = "foundation-iot-verified-rewards/";
+const &str prefix = "foundation-iot-verified-rewards/";
 
 #[derive(Debug, Clone)]
 pub struct FileStore {
