e81643a4bfefda835d32f1e847399fa60904f749
Matthew Patrick <64496719+mrpatrick1991@users.noreply.github.com>
2025-01-21 11:16:49 -0700

    Update file_store.rs
    add options for request_payer

--- file_store/src/file_store.rs
+++ file_store/src/file_store.rs
@@ -112,6 +112,7 @@ impl FileStore {
             .list_objects_v2()
             .bucket(&self.bucket)
             .prefix(file_type.to_string())
+            .request_payer("requester")
             .set_start_after(after.map(|dt| FileInfo::from((file_type, dt)).into()));
 
         futures::stream::unfold(
@@ -168,6 +169,7 @@ impl FileStore {
                 .key(file.file_name().map(|name| name.to_string_lossy()).unwrap())
                 .body(byte_stream)
                 .content_type("application/octet-stream")
+                .request_payer("requester")
                 .send()
                 .map_ok(|_| ())
                 .map_err(Error::s3_error)
@@ -182,6 +184,7 @@ impl FileStore {
                 .delete_object()
                 .bucket(&self.bucket)
                 .key(key)
+                .request_payer("requester")
                 .send()
                 .map_ok(|_| ())
                 .map_err(Error::s3_error)
@@ -268,6 +271,7 @@ where
         .get_object()
         .bucket(bucket)
         .key(key)
+        .request_payer("requester")
         .send()
         .map_ok(|output| output.body)
         .map_err(Error::s3_error)
