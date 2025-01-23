51a5938798b162356a99443ca389825d0fb8d81c
Matthew Patrick <64496719+mrpatrick1991@users.noreply.github.com>
2025-01-23 09:37:18 -0700

    Update file_store.rs

--- file_store/src/file_store.rs
+++ file_store/src/file_store.rs
@@ -193,7 +193,6 @@ impl FileStore {
                 .await
         )
     }
-
     pub async fn get_raw<K>(&self, key: K) -> Result<ByteStream>
     where
         K: Into<String> + std::fmt::Display,
@@ -203,11 +202,11 @@ impl FileStore {
 
     pub async fn get<K>(&self, key: K) -> Result<BytesMutStream>
     where
-        K: Into<String>,
+        K: Into<String> + std::fmt::Display,
     {
         Ok(stream_source(self.get_raw(key).await?))
     }
-
+    
     /// Stream a series of ordered items from the store from remote files with
     /// the given keys.
     pub fn source(&self, infos: FileInfoStream) -> BytesMutStream {
